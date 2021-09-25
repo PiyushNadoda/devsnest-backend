a85128cc8f2aac12c9b416bced420e9d3830a874
PiyushNadoda <910piyushsindhav@gmail.com>
2021-09-26 01:32:35 +0530

    changes

---
+++ Day01/README.md
@@ -0,0 +1,41 @@
+# Day 1
+## Setup Redis and PostgreSQL in your local
+
+### 1. Redis
+#### Installation
+- on Arch
+```bash
+yay -S redis
+```
+- on Ubuntu
+```bash
+sudo apt install redis-server
+```
+
+#### Start service
+```bash
+sudo service redis-server start
+redis-cli
+```
+
+<h1></h1>
+
+### 2. PostgreSQL
+#### Installation
+- on Arch
+```bash
+yay -S postgresql
+sudo su - postgres
+initdb --locale en_US.UTF-8 -D /var/lib/postgres/data
+exit
+```
+- on Ubuntu
+```bash
+sudo apt install postgresql postgresql-contrib
+```
+#### Start postgreSQL
+```bash
+sudo systemctl start postgresql
+sudo -iu postgres
+psql
+```
---
+++ Day05/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const args = process.argv;
+
+// for creating new directory
+if(args[2] === '--mkdir'){
+    fs.mkdirSync(args[3])
+}
+
+// for removing directory
+if(args[2] === '--rmdir'){
+    fs.rmdirSync(args[3])
+}
+
+// rename directory
+if(args[2] === '--rename'){
+    fs.renameSync(args[3], args[4])
+}
+
+// create new file
+if(args[2] === '--touch'){
+    fs.writeFileSync(args[3], args[4])
+}
+
+// append file create new file if not exist
+if(args[2] === '--append-text'){
+    fs.appendFileSync(args[3], args[4])
+}
+
+// read the file
+if(args[2] === '--read'){
+    fs.readFileSync(args[3], 'utf-8')
+}
+
+// remove file
+if(args[2] === '--remove'){
+    fs.unlinkSync(args[3])
+}
