d7f419787275decea17ba915973ad93f764a172c
PiyushNadoda <910piyushsindhav@gmail.com>
2021-10-06 00:53:34 +0530

    ✏️ Fixed typo

--- Day11-12/models/mongo.js
+++ Day11-12/models/mongo.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    fullName: {type: 'string', default: ''},
-    email: {type: 'string', default: ''},
-    password: {type: 'string', default: ''},
+    fullName: {type: String, default: ''},
+    email: {type: String, default: ''},
+    password: {type: String, default: ''},
 })
 
 module.exports = User = mongoose.model('model', UserSchema);
