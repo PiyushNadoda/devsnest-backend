bbc7f9bb3b19bcbf580a9268d4424fe5a4e76529
PiyushNadoda <910piyushsindhav@gmail.com>
2021-10-06 19:00:24 +0530

    🐛 Fixed bugs

--- Day11-12/controllers/register.js
+++ Day11-12/controllers/register.js
@@ -1,6 +1,16 @@
 const User = require("../models/mongo");
 const bcrypt = require("bcrypt");
 
+
+
+function validateEmailAccessibility(email){
+
+  return User.findOne({email: email}).then(function(result){
+    console.log(result);
+       return result !== null;
+  });
+}
+
 const saltRounds = 10;
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -8,14 +18,13 @@ const register = async (req, res) => {
     const alreadyExists = await User.findOne({ where: { email } });
     if (alreadyExists) {
       res.status(401).send("Email already exists.");
-    }
+    } else {
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(password, salt);
-
     const newUser = new User({email: email.toLowerCase(), password: hash, fullName: "Piyush"})
     const savedUser = await newUser.save();
     res.status(201).send(savedUser)
-  } catch (err) {
+  } }catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong")
   }
--- Day11-12/database/mongo.js
+++ Day11-12/database/mongo.js
@@ -6,4 +6,6 @@ mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true
 
 var db = mongoose.connection;
 
+db.auth('admin', '987654321')
+
 db.on("error", console.error.bind(console, "Mongodb connection error"));
